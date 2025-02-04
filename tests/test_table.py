f684c29afec88a3c5bb694b056882610a9e99547
daniel.cahall <danielenricocahall@gmail.com>
2025-02-03 21:41:25 -0500

    feature: factory method for tables

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,23 +1,27 @@
 import pytest
 
-from pyquerybuilder.table import Table, InvalidFieldsException
+from pyquerybuilder.table import InvalidFieldsException, MySQLTable
 
 
 def test_table_select():
-    table = Table("test_table", "test_field", "another_test_field")
+    table = MySQLTable("test_table", "test_field", "another_test_field")
+    # table = Table("test_table", "test_field", "another_test_field")
     query = table.select("test_field").build()
-    assert query == "SELECT test_field FROM test_table"
+    assert query == "SELECT `test_field` FROM `test_table`"
 
 
 def test_table_non_existent_field():
-    table = Table("test_table", "test_field", "another_test_field")
+    table = MySQLTable("test_table", "test_field", "another_test_field")
     with pytest.raises(InvalidFieldsException):
         table.select("some_nonexistent_test_field")
 
 
 def test_table_with_schema():
-    table = Table(
+    table = MySQLTable(
         "test_table", "test_field", "another_test_field", schema="test_schema"
     )
     query = table.select("test_field", "another_test_field").build()
-    assert query == "SELECT test_field,another_test_field FROM test_schema.test_table"
+    assert (
+        query
+        == "SELECT `test_field`,`another_test_field` FROM `test_schema.test_table`"
+    )
