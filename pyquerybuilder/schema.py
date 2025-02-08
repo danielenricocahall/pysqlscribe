7a1170d2e1adf4cd7c7cd437a5364e38585555f6
daniel.cahall <danielenricocahall@gmail.com>
2025-02-08 09:49:29 -0500

    feature: add schema capabilities

--- pyquerybuilder/schema.py
+++ pyquerybuilder/schema.py
@@ -6,6 +6,17 @@ from pyquerybuilder.table import Table
 class Schema:
     def __init__(self, name: str, tables: List[Table | str] | None = None):
         self.name = name
-        if all(isinstance(table, str) for table in tables):
-            tables = [Table(table_name, schema=self.name) for table_name in tables]
         self.tables = tables
+
+    @property
+    def tables(self):
+        return self._tables
+
+    @tables.setter
+    def tables(self, tables_: list[str | Table]):
+        if all(isinstance(table, str) for table in tables_):
+            tables_ = [Table(table_name) for table_name in tables_]
+        for table in tables_:
+            setattr(self, table.name, table)
+            table.schema = self.name
+        self._tables = tables_
--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,5 +1,5 @@
 from pyquerybuilder.schema import Schema
-from pyquerybuilder.table import Table
+from pyquerybuilder.table import Table, OracleTable
 
 
 def test_schema_create_tables():
@@ -7,3 +7,16 @@ def test_schema_create_tables():
     assert len(schema.tables) == 2
     assert all(isinstance(table, Table) for table in schema.tables)
     assert all(table.name.startswith("test_schema") for table in schema.tables)
+    assert hasattr(schema, "test_table")
+
+
+def test_schema_supply_tables():
+    tables = [
+        OracleTable("cards", "stars", "card_name", "card_description", "card_cost"),
+        OracleTable("regions", "region_id", "region_name"),
+    ]
+    schema = Schema("kaibacorp", tables)
+    assert len(schema.tables) == 2
+    assert all(isinstance(table, Table) for table in schema.tables)
+    assert all(table.name.startswith("kaibacorp") for table in schema.tables)
+    assert hasattr(schema, "cards")
