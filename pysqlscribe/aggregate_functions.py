633d2cf886744144dd6da1c3bb2c2702a5f1bd26
daniel.cahall <danielenricocahall@gmail.com>
2025-03-03 06:12:58 -0500

    feature: use ExpressionColumn for agg functions

--- pysqlscribe/aggregate_functions.py
+++ pysqlscribe/aggregate_functions.py
@@ -1,11 +1,11 @@
-from pysqlscribe.column import Column
+from pysqlscribe.column import Column, ExpressionColumn
 from pysqlscribe.functions import AggregateFunctions
 
 
 def _aggregate_function(agg_function: str, column: Column | str | int):
     if not isinstance(column, Column):
         return f"{agg_function}({column})"
-    return Column(f"{agg_function}({column.name})", column.table_name)
+    return ExpressionColumn(f"{agg_function}({column.name})", column.table_name)
 
 
 def max_(column: Column | str) -> Column | str:
