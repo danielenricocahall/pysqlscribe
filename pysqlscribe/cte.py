ef16a7c9ef6de7e991546cc8eec4fc23df870227
daniel.cahall <danielenricocahall@gmail.com>
2026-04-19 12:52:08 -0400

    add multi-cte support and exceptions for empty/dupes

--- pysqlscribe/cte.py
+++ pysqlscribe/cte.py
@@ -1,5 +1,6 @@
 from typing import Self
 
+from pysqlscribe.exceptions import DuplicateCTENameException, EmptyCTEException
 from pysqlscribe.query import Query
 
 WITH = "WITH"
@@ -10,22 +11,39 @@ AS = "AS"
 class With(Query):
     """Builder for WITH ... AS ... expressions."""
 
-    def __init__(self, cte_name: str, dialect: str = None, recursive: bool = False):
+    def __init__(self, cte_name: str, dialect: str, recursive: bool = False):
         super().__init__(dialect)
-        self._cte_name = cte_name
+        self._current_cte_name = cte_name
         self.recursive = recursive
+        self._subqueries = {}
 
     def as_(self, subquery: str | Query) -> Self:
         if isinstance(subquery, Query):
             subquery = str(subquery)
-        self._subquery = f" {AS} ({subquery})"
+        self._subqueries[self._current_cte_name] = subquery
         return self
 
     def build(self, clear: bool = True) -> str:
+        if not self._subqueries:
+            raise EmptyCTEException(
+                f"No subqueries defined for WITH clause '{self._current_cte_name}'"
+            )
         query = super().build(clear=clear)
-        cte_query = f"{WITH if not self.recursive else WITH_RECURSIVE} {self._cte_name}{self._subquery} {query}"
-        return cte_query
+        with_block = f"{WITH if not self.recursive else WITH_RECURSIVE}"
+        cte_queries = ", ".join(
+            f"{cte_name} {AS} ({subquery})"
+            for cte_name, subquery in self._subqueries.items()
+        )
+        return f"{with_block} {cte_queries} {query}"
+
+    def with_(self, cte_name: str) -> Self:
+        if cte_name in self._subqueries:
+            raise DuplicateCTENameException(
+                f"CTE with name '{cte_name}' already exists in this context"
+            )
+        self._current_cte_name = cte_name
+        return self
 
 
-def with_(cte_name: str, dialect: str = None, recursive: bool = False):
+def with_(cte_name: str, dialect: str, recursive: bool = False):
     return With(cte_name, dialect, recursive)
--- tests/test_cte.py
+++ tests/test_cte.py
@@ -1,5 +1,8 @@
+import pytest
+
 from pysqlscribe.aggregate_functions import avg
 from pysqlscribe.cte import With, with_
+from pysqlscribe.exceptions import DuplicateCTENameException, EmptyCTEException
 from pysqlscribe.query import Query
 
 
@@ -21,6 +24,29 @@ def test_basic_cte():
     )
 
 
+def test_multi_cte():
+    user_query_builder = Query("mysql")
+    user_query_builder.select("name", "id").from_("users").where("city = 'New York'")
+    vehicle_query_builder = Query("mysql")
+    vehicle_query_builder.select("vehicle_type", "vehicle_id", "owner_id").from_(
+        "vehicles"
+    ).where("city = 'New York'")
+    cte_query = (
+        With("UsersNY", dialect="mysql")
+        .as_(user_query_builder)
+        .with_("VehiclesNY")
+        .as_(vehicle_query_builder)
+        .select("name", "vehicle_type")
+        .from_("UsersNY")
+        .join("VehiclesNY", condition="id = owner_id")
+        .build()
+    )
+    assert (
+        cte_query
+        == "WITH UsersNY AS (SELECT `name`, `id` FROM `users` WHERE city = 'New York'), VehiclesNY AS (SELECT `vehicle_type`, `vehicle_id`, `owner_id` FROM `vehicles` WHERE city = 'New York') SELECT `name`, `vehicle_type` FROM `UsersNY` INNER JOIN `VehiclesNY` ON id = owner_id"
+    )
+
+
 def test_cte_function():
     query_builder = Query("mysql")
     query_builder.select("department", avg("salary")).from_("employees").group_by(
@@ -37,3 +63,19 @@ def test_cte_function():
         cte_query
         == "WITH AvgSalaryByDepartment AS (SELECT `department`, AVG(salary) FROM `employees` GROUP BY `department`) SELECT * FROM `AvgSalaryByDepartment`"
     )
+
+
+def test_duplicate_cte_fails():
+    query_builder = Query("mysql")
+    query_builder.select("department", avg("salary")).from_("employees").group_by(
+        "department"
+    )
+    with pytest.raises(DuplicateCTENameException):
+        With("AvgSalaryByDepartment", dialect="postgres").as_(query_builder).with_(
+            "AvgSalaryByDepartment"
+        ).as_(query_builder).select("*").build()
+
+
+def test_empty_cte_fails():
+    with pytest.raises(EmptyCTEException):
+        With("AvgSalaryByDepartment", dialect="postgres").build()
